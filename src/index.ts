73d44dc3454f9c0eb3699d7ebabf91512684606d
Victhor Araújo <victhor.araujo.41@gmail.com>
2021-08-21 00:26:46 -0300

    solve challenge

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import express, { NextFunction, Response, Request } from "express";
+import swaggerUi from "swagger-ui-express";
 
 import "express-async-errors";
 
 import { AppError } from "./errors/AppError";
 import { usersRoutes } from "./routes/users.routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use("/users", usersRoutes);
 
